5d115e3bec146f9d49d4c3bf914e8cfe4bb7c340
Debashis B. Jupiter <jupiter_104@ymail.com>
2019-03-28 12:02:17 +0530

    Add files via upload

---
+++ GuessTheNumber/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace GuessTheNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int secretNumber = random.Next(1, 10);
+
+            Console.WriteLine("Guess the secret number between 1 and 10");
+
+            for (int i = 1; i < 5; i++)
+            {
+                string input = Console.ReadLine();
+                int number = Convert.ToInt32(input);
+
+                if (number == secretNumber)
+                {
+                    Console.WriteLine("You guessed correctly!");
+                    break;
+                }
+                else
+                    Console.WriteLine("Try again!");
+            }
+            Console.Read();
+        }
+    }
+}
